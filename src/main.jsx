f32d1db2056256a06afd21b3adf7378c07823dab
CamiloSR001 <juanromero755@outlook.com>
2025-11-09 13:51:32 -0500

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {}
       <AuthProvider>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter> {}
   </React.StrictMode>
 );
+
